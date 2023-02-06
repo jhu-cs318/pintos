3f5a7ffb8309ce185be55848915d1b75c61cb18a
Nadun Kumarasinghe <59087013+codened@users.noreply.github.com>
2023-02-06 22:42:43 +0530

    comments added

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,10 +6,7 @@
 
 static void syscall_handler (struct intr_frame *);
 
-void syscall_init (void) 
-{
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
-}
+
 #include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
@@ -527,30 +524,7 @@ buf_validator(const void* buf, unsigned byte_size) {
 
 int
 get_page(const void *vaddr) {
-  void *ptr = pagedir_get_page(thread_current()->pagedir, vaddr);
-
-  if (!ptr)
-    exit(SYS_ERROR);
-
-  return (int)ptr;
-}
-
-
-struct child_process* find_child(int pid) {
-
-  struct thread *t = thread_current();
-  struct list_elem *e;
-  struct list_elem *next;
-  
-  for (e = list_begin(&t->child_list); e != list_end(&t->child_list); e = next) {
-    next = list_next(e);
-    struct child_process *child_pr = list_entry(e, struct child_process, elem);
-
-    if (pid == child_pr->pid)
-      return child_pr;
-  }
-
-  return NULL;
+ 
 }
 
 
@@ -593,3 +567,4 @@ close_file (int file_descriptor) {
     }
   }
 }
+
